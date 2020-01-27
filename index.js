857d3e3f10f1dd86dec4cb39552a1fffdda4fbe7
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-01-27 13:19:16 -0700

    Write `makeSmartPerson` function

--- index.js
+++ index.js
@@ -77,8 +77,16 @@ function getName(namedObj) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  return {
+      name: name,
+      sum: function(a, b) {
+          return a + b;
+      },
+      speak: function() {
+          return `Hello, my name is ${this.name}`
+      },
+  };
 }
 
 
