4bfe03151b59dd121da13dfbe63968639bc1adf8
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-01-27 13:46:39 -0700

    Write `carMaker` function

--- index.js
+++ index.js
@@ -302,8 +302,14 @@ const argTimesTwo = (x) => x * 2; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(distance) {
+    return {
+        odometer: distance,
+        drive: function(distance) {
+            this.odometer += distance;
+            return this.odometer;
+        }
+    }
 }
 
 /// ////// END OF CHALLENGE /////////
